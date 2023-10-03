431d345bdb9349ed8dfde60185e6d89b5537dd0f
Érico Augusto <67467324+Erico-AS@users.noreply.github.com>
2023-10-03 15:42:40 -0300

    Projeto

--- pratica-09/src/pratica.ts
+++
@@ -1,9 +0,0 @@
-const arrow = (a: number, b: number): number => a + b;
-
-type recebe = (arrow() : Number => Number) => Number
-
-function printar(recebe:recebe, a:Number, b:Number) {
-    console.log(a,b)
-}
-
-let arrow
